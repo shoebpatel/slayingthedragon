1fa02fbdc7188970f24541236a4f731126b62fd5
mohdpatel <shoeb.patel@nagarro.com>
2024-10-15 20:38:43 +0530

    fixed mobile nav bug

--- src/components/mobile-nav.js
+++ src/components/mobile-nav.js
@@ -1,22 +1,15 @@
-export const openMobileNav = () => {
+export default (action) => {
   const mobileMenu = document.querySelector(".mobile-menu");
-  mobileMenu.style.display = "flex";
-  const navBars = document.querySelector(".mobile-nav-bars");
-  navBars.style.display = "none";
   const titleFlex = document.querySelector(".title-flex");
-  titleFlex.style.display = "none";
-  const body = document.getElementsByTagName("body");
-  body[0].style.overflow = "hidden";
-}
 
-export const closeMobileNav = () => {
-  const mobileMenu = document.querySelector(".mobile-menu");
+  if (action === 'open') {
+    mobileMenu.style.display = "flex";
+    titleFlex.style.display = "none";
+    document.body.style.overflowY = "hidden";
+  }
+  if (action === "close") {
     mobileMenu.style.display = "none";
-    const navBars = document.querySelector(".mobile-nav-bars");
-    navBars.style.display = "block";
-    const titleFlex = document.querySelector(".title-flex");
     titleFlex.style.display = "flex";
-    const body = document.getElementsByTagName("body");
-    body[0].style.overflow = "visible";
-}
-
+    document.body.style.overflowY = "auto";
+  }
+};
--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-import '../styles/normalize.css'
-import '../styles/style.css'
-import '../styles/header.css'
-import '../styles/utils.css'
+import '../styles/normalize.css';
+import '../styles/style.css';
+import '../styles/header.css';
+import '../styles/utils.css';
 
-import {openMobileNav, closeMobileNav} from "../src/components/mobile-nav";
+import mobileNav from "../src/components/mobile-nav";
 
-window.openMobileNav = openMobileNav;
-window.closeMobileNav = closeMobileNav;
+window.mobileNav = mobileNav;
