e5bfb207410f059d9dcbe04247b50ad519d01f57
mohdpatel <shoeb.patel@nagarro.com>
2024-10-28 20:12:28 +0530

    added courses sections

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import '../styles/normalize.css';
 import '../styles/style.css';
 import '../styles/header.css';
 import '../styles/intro.css';
+import '../styles/courses.css';
 import '../styles/utils.css';
 
 import mobileNav from "../src/components/mobile-nav";
