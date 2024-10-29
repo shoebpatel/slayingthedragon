46a941f5a57e32c69b10a4ea08eeade7e3b53021
mohdpatel <shoeb.patel@nagarro.com>
2024-10-29 20:14:55 +0530

    added about sections

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import '../styles/style.css';
 import '../styles/header.css';
 import '../styles/intro.css';
 import '../styles/courses.css';
+import '../styles/about.css';
 import '../styles/utils.css';
 
 import mobileNav from "../src/components/mobile-nav";
