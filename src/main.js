a70e971b217641465d2bf42a9cb6860dd950069a
mohdpatel <shoeb.patel@nagarro.com>
2024-11-02 19:26:40 +0530

    added support sections

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import '../styles/header.css';
 import '../styles/intro.css';
 import '../styles/courses.css';
 import '../styles/about.css';
+import '../styles/support.css';
 import '../styles/utils.css';
 
 import mobileNav from "../src/components/mobile-nav";
